28ca384cf54015bc98b1ef64000db1b2005ffd5c
Leonardo Camargo <leonardorcamargo@gmail.com>
2020-12-22 21:43:58 -0300

    :sparkles: Create new test route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world!");
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  return res.json({
+    message: 'Mistery-friend API is OK',
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.info(`API is running on port ${process.env.PORT || 3000} 🚀`);
+});
